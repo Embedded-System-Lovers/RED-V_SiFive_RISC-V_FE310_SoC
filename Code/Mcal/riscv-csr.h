0e553f1e77e248d68d6e3b6fea62cae946530f79
Christopher Kormanyos <ckormanyos@yahoo.com>
2024-05-30 07:10:46 +0200

    Repair build and syntax

--- Code/Mcal/riscv-csr.h
+++ Code/Mcal/riscv-csr.h
@@ -9,6 +9,14 @@
 #ifndef RISCV_CSR_H
 #define RISCV_CSR_H
 
+#if defined(__GNUC__)
+#if defined(__cplusplus)
+asm(".option arch, +zicsr");
+#else
+__asm(".option arch, +zicsr");
+#endif
+#endif
+
 #include <stdint.h>
 
 #if __riscv_xlen==32
@@ -21,10 +29,6 @@ typedef uint32_t uint_csr32_t;
 typedef uint64_t uint_csr64_t;
 #else
 #error "Unknown XLEN"
-#endif
-
-#if ((defined(__GNUC__)  && (__GNUC__ > 10)) && defined(__riscv))
-asm(".option arch, +zicsr");
 #endif
 
 /*******************************************
